68b40d2c83a8b345a2b386420c04c2322c888491
Robin Gjølstad <robingjolstad@gmail.com>
2019-02-17 18:58:47 +0100

    Apparently last commit didn't function as expected. Added source files for user-definitions

---
+++ lib/user/user.cpp
@@ -0,0 +1,12 @@
+/*
+ * Created on: 17.02.2019
+ * Author:     Robin Gjølstad
+ * 
+ * This source file should contain user created function definitions.
+ * 
+ */
+
+#include "user.h"
+#include <stdio.h>
+
+
---
+++ lib/user/user.h
@@ -0,0 +1,18 @@
+/*
+ * Created on: 17.02.2019
+ * Author:     Robin Gjølstad
+ * 
+ * This header file should contain: 
+ *  - User defined macros
+ *  - User declared functions
+ *  - User declared global variables
+ * 
+ * These declarations should be used and further defined in user.cpp
+ */
+
+#ifndef USER_H_
+#define USER_H_
+
+
+
+#endif /* USER_H_ */
